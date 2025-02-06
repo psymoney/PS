9198791b79780f95799cc69f8cae0c5168d94be7
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2025-02-06 21:57:29 +0900

    Time: 79 ms (25.00%) | Memory: 11.4 MB (25.00%) - LeetSync

---
+++ LeetCode/Medium/1364-tuple-with-same-product/tuple-with-same-product.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn tuple_same_product(nums: Vec<i32>) -> i32 {
+        let mut answer: i32 = 0;
+        let mut map = HashMap::<i32, i32>::new();
+        let mut target_map = HashMap::<i32, i32>::new();
+
+        for (i, vi) in nums.iter().enumerate() {
+            for (_, vj) in nums.iter().enumerate().skip(i + 1) {
+                let product: i32 = vi * vj;
+                map.entry(product)
+                    .and_modify(|count| {
+                        *count += 1;
+
+                        if *count > 1 {
+                            target_map
+                                .entry(product)
+                                .and_modify(|count| *count += 1)
+                                .or_insert(2);
+                        }
+                    })
+                    .or_insert(1);
+            }
+        }
+
+        for v in target_map.values() {
+            answer += *v * (*v - 1) / 2;
+        }
+
+        answer * 8
+    }
+}
