21afb6f860abe5a65f369bc9931c7cdc7300dec6
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2025-02-09 13:29:15 +0900

    Time: 75 ms (66.90%) | Memory: 38.9 MB (28.81%) - LeetSync

--- LeetCode/Medium/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
+++ LeetCode/Medium/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -1,19 +1,13 @@
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        answer: int = len(nums) * (len(nums) - 1) // 2
+        n: int = len(nums)
+        answer: int = n * (n - 1) // 2
         frequency: dict[int, int] = {}
-        effective_frequency_keys: set[int] = set()
 
         for idx, val in enumerate(nums):
             key = idx - val
-            frequency.setdefault(key, 0)
-            frequency[key] += 1
-            if frequency[key] > 1:
-                effective_frequency_keys.add(key)
+            answer -= frequency.get(key, 0)
+            frequency[key] = frequency.get(key, 0) + 1
 
-        for key in effective_frequency_keys:
-            val = frequency[key]
-            answer -= val * (val - 1) // 2
-        
         return answer
         
