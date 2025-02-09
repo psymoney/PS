0af4bd9dc05ba3653b8985bb09696502ca9192e2
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2025-02-09 13:08:05 +0900

    Time: 50 ms (0.00%) | Memory: 7.4 MB (50.00%) - LeetSync

---
+++ LeetCode/Medium/2448-count-number-of-bad-pairs/count-number-of-bad-pairs.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn count_bad_pairs(nums: Vec<i32>) -> i64 {
+        let mut answer = (nums.len() * (nums.len() - 1) / 2) as i64;
+        let mut frequency = HashMap::<i32, usize>::new();
+
+        for (index, value) in nums.iter().enumerate() {
+            let key = index as i32 - *value;
+            *frequency.entry(key).or_insert(0) += 1;
+        }
+        println!("{:?}", frequency);
+        for value in frequency.values() {
+            if *value > 1 {
+                answer -= (*value * (*value - 1) / 2) as i64;
+            }
+        }
+
+        answer
+    }
+}
