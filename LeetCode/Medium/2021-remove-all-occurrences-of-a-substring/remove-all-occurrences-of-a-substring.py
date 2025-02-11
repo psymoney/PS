c713046d1a5cea9bb40aef58e9022923574ee9a3
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2025-02-11 13:07:50 +0900

    Time: 3 ms (56.31%) | Memory: 17.7 MB (75.36%) - LeetSync

--- LeetCode/Medium/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ LeetCode/Medium/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -2,15 +2,6 @@ from collections import deque
 
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        n = len(part)
-        q = deque([])
-
-        for c in s:
-            q.append(c)
-            if len(q) >= n:
-                comp_str = "".join(list(q)[-n:])
-                if comp_str == part:
-                    for _ in range(n):
-                        q.pop()
-
-        return "".join(list(q))
+        while part in s:
+            s = s.replace(part, "", 1)
+        return s
