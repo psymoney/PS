230b9b9035802e7de0216c6c30784c0ff762d627
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2025-02-12 21:12:43 +0900

    Time: 2 ms (79.41%) | Memory: 4.1 MB (20.59%) - LeetSync

--- LeetCode/Medium/2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.rs
+++ LeetCode/Medium/2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.rs
@@ -1,8 +1,9 @@
-use std::collections::{HashMap, BinaryHeap};
+use std::cmp;
 
 impl Solution {
     pub fn maximum_sum(nums: Vec<i32>) -> i32 {
-        let mut freq: HashMap<usize, (usize, BinaryHeap<i32>)> = HashMap::new();
+        let mut max = [0; 82];
+        let mut answer: i32 = -1;
 
         for num in nums {
             let mut rem: usize = (num % 10) as usize;
@@ -15,26 +16,12 @@ impl Solution {
                 sum += rem;
             }
 
-            freq.entry(sum)
-                .and_modify(|(f, heap)| {
-                    *f += 1;
-                    heap.push(num);
-                })
-                .or_insert((1, BinaryHeap::from(vec![num])));
+            if (max[sum] != 0) {
+                answer = cmp::max(answer, num + max[sum]);
+            }
+            max[sum] = cmp::max(max[sum], num);
         }
 
-        freq.values_mut()
-            .filter_map(|(f, heap)| {
-                if *f > 1 {
-                    let first = heap.pop()?;
-                    let second = heap.pop()?;
-                    Some(first + second)
-                } else {
-                    None
-                }
-            })
-            .max()
-            .unwrap_or(-1)
+        answer
     }
-
 }
