906d88caae8146229b3f8ddb47fc96d24ed11a49
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2025-02-20 21:10:15 +0900

    Time: 0 ms (100.00%) | Memory: 2.2 MB (57.14%) - LeetSync

--- LeetCode/Medium/2107-find-unique-binary-string/find-unique-binary-string.rs
+++ LeetCode/Medium/2107-find-unique-binary-string/find-unique-binary-string.rs
@@ -2,28 +2,17 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn find_different_binary_string(nums: Vec<String>) -> String {
-        let n: usize = nums.len();
-        let strings: HashSet<String> = nums.into_iter().collect();
-        let mut ans: String = String::new();
+        let mut s: String = String::new();
 
-        Self::build_and_find_unique_string(String::new(), n, &mut ans, &strings);
-
-        ans
-    }
-
-    fn build_and_find_unique_string(s: String, n: usize, ans: &mut String, strings: &HashSet<String>) {
-        if s.len() == n {
-            if !strings.contains(&s) {
-                *ans = s;
+        for (i, bs) in nums.into_iter().enumerate() {
+            let c = bs.chars().nth(i).unwrap();
+            if c == '0' {
+                s.push('1');
+            } else {
+                s.push('0');
             }
-            return;
         }
 
-        for c in ['0', '1'] {
-            let mut new_s: String = s.clone();
-            new_s.push(c);
-            Self::build_and_find_unique_string(new_s, n, ans, strings);
-        }
+        s
     }
-
 }
