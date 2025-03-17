7fff7df580e07d1febab086c8162e43258965690
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2025-03-17 20:53:21 +0900

    Time: 0 ms (100.00%) | Memory: 2.2 MB (100.00%) - LeetSync

--- LeetCode/Medium/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.rs
+++ LeetCode/Medium/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.rs
@@ -2,23 +2,16 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn divide_array(nums: Vec<i32>) -> bool {
-        let mut counter_map: HashMap<i32, usize> = HashMap::new();
-
-        for n in nums {
-            let mut count: &mut usize = counter_map.entry(n).or_insert(0);
-            if *count == 1 {
-                *count -= 1;
-            } else if *count == 0 {
-                *count += 1;
-            }
+        let mut nums: Vec<i32> = nums.to_owned();
+        nums.sort();
+        if nums.len() % 2 != 0 {
+            return false;
         }
-
-        for c in counter_map.values() {
-            if *c != 0 {
+        for i in (1..nums.len()).step_by(2) {
+            if nums[i - 1] != nums[i] {
                 return false;
             }
         }
-
         true
     }
 }
