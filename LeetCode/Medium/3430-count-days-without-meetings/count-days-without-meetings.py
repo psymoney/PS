1c2745fc3f954e0f55871a3ecc518077987a0292
Sangyeop Kim <78402001+psymoney@users.noreply.github.com>
2025-03-24 20:56:30 +0900

    Time: 118 ms (84.38%) | Memory: 52.7 MB (86.15%) - LeetSync

--- LeetCode/Medium/3430-count-days-without-meetings/count-days-without-meetings.py
+++ LeetCode/Medium/3430-count-days-without-meetings/count-days-without-meetings.py
@@ -1,15 +1,16 @@
+from collections import deque
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
         if len(meetings) == 1:
             return days - (meetings[0][1] - meetings[0][0] + 1)
 
-        meetings = sorted(meetings, key=lambda x: x[0])
+        meetings = deque(sorted(meetings, key=lambda x: x[0]))
         s, e = None, None
 
         while len(meetings):
             if not s and not e:
-                s, e = meetings.pop(0)
-            ns, ne = meetings.pop(0)
+                s, e = meetings.popleft()
+            ns, ne = meetings.popleft()
 
             if e < ns - 1:
                 days -= (e - s + 1)
